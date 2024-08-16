52a634ce064d4912ea52061a9fb25185c97710e5
SALAMSASA <fqqadh@gmail.com>
2024-08-17 00:33:09 +0300

    Update auto.py

--- YousefMusic/plugins/play/auto.py
+++ YousefMusic/plugins/play/auto.py
@@ -1,27 +1,46 @@
-import os
-import asyncio
-import requests
-import config
 import random
-import time
+import string
+import asyncio
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InputMediaPhoto
+from pytgcalls.exceptions import NoActiveGroupCall
 from config import START_IMG_URL
-from pyrogram import filters
-from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from pyrogram.types import (
-    CallbackQuery,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    InputMediaPhoto,
-    Message,
+import config
+from AlexaMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from AlexaMusic.core.call import Alexa
+from AlexaMusic.utils import seconds_to_min, time_to_seconds
+from AlexaMusic.utils.channelplay import get_channeplayCB
+from AlexaMusic.utils.database import is_video_allowed, is_served_user, get_served_chats
+from AlexaMusic.utils.decorators.language import languageCB, LanguageStart
+from AlexaMusic.utils.decorators.play import PlayWrapper
+from AlexaMusic.utils.formatters import formats
+from AlexaMusic.utils.inline.play import (
+    livestream_markup,
+    playlist_markup,
+    slider_markup,
+    track_markup,
 )
-from strings.filters import command
-from YousefMusic.utils.decorators import AdminActual
-from YousefMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from YousefMusic import app
-from random import  choice, randint
+from AlexaMusic.utils.inline.playlist import botplaylist_markup
+from AlexaMusic.utils.logger import play_logs
+from AlexaMusic.utils.stream.stream import stream
+from config import BANNED_USERS, lyrical, CHANNEL_SUDO, YAFA_NAME, YAFA_CHANNEL
+from strings import get_command, get_string
+from AlexaMusic.misc import SUDOERS
+from AlexaMusic.plugins.play.playlist import del_plist_msg
+from AlexaMusic.plugins.sudo.sudoers import sudoers_list
+from AlexaMusic.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_assistant,
+    get_lang,
+    get_userss,
+    is_on_off,
+    is_served_private_chat,
+)
+from AlexaMusic.utils.inline import help_pannel, private_panel, start_pannel
+from AlexaMusic.utils.command import commandpro
+from youtubesearchpython.__future__ import VideosSearch
 
 MESSAGE = f"""- اقوي بوت ميوزك قنوات و جروبات سرعه وجوده خارقه
 
